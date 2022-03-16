1bb95af0a9e242e24f615c3adf3d4f221ffc5343
soarecor <cornell109@gmail.com>
2022-03-16 17:38:39 -0400

    longest common Prefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let splitStrings = [];
+    let maxLength = 0;
+    let equalLetters = [];
+      
+    if(strs.length === 1) return strs[0]
+  
+    strs.forEach((str) => {
+      if (str.length > maxLength) maxLength = str.length;
+      const splitStr = str.split('');
+      splitStrings.push(splitStr);
+    });
+  
+    for (let i = 0; i < splitStrings.length; i++) {
+      //compare current splitStr and next splitStr, see if there are more than one occurence of prefix
+      if (i === 1 && equalLetters.length === 0) break
+      for (let j = 0; j < maxLength; j++) {
+        let nextLetter = () => {
+          if (!splitStrings[i + 1]) return '';
+          else {
+            if (splitStrings[i + 1][j]) return splitStrings[i + 1][j];
+            else return '';
+          }
+        };
+        let isLetterEqual = splitStrings[i][j] === nextLetter();
+        if (isLetterEqual) equalLetters.push(splitStrings[i][j]);
+      }
+    }
+  
+    if(equalLetters.length > 1) {
+      equalLetters = equalLetters.filter((letter, index) => {
+        return equalLetters.slice(index + 1).includes(letter);
+      });
+    }
+  
+    return equalLetters.join('') || ''
+  };
+
+
+  console.log(longestCommonPrefix(["flower","flow","flight"])
